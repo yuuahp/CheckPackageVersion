0410fdfd57702835734d3b8c2ae56b9dc33f016d
yuuaHP <yuuahp@icloud.com>
2021-12-27 12:48:04 +0900

    feat: replaceVersion

--- README.md
+++ README.md
@@ -8,11 +8,14 @@
 //config.ts
 export default {
     token: "ここにアクセストークン",
+    groupId: "パッケージのGroupID",
     name: "パッケージのお名前",
-    type: "パッケージのおタイプ(maven/npm/rubygems/docker/nuget/container)"
+    type: "パッケージのおタイプ(maven/npm/rubygems/docker/nuget/container)",
+    replaceVersion: true, //指定したビルドファイルを最新バージョンに書き換えるか (KotlinDSLのみ対応)
+    buildFilePath: "build.gradle.ktsへのパス"
 }
 ```
 
 ## 実行
 
-`npm test`
+`npm run`
--- index.ts
+++ index.ts
@@ -1,12 +1,13 @@
 import { Octokit } from "@octokit/core"
 import config from "./config";
+import * as fs from 'fs';
 
 const octokit = new Octokit({ auth: config.token });
 
-async function getLatestVersion(type: string, name: string) {
+async function getLatestVersion(type: string, groupId: string, name: string) {
     const latest = (await octokit.request('GET /user/packages/{package_type}/{package_name}/versions', {
         package_type: type as "maven" | "npm" | "rubygems" | "docker" | "nuget" | "container",
-        package_name: name
+        package_name: `${groupId}.${name}`
     })).data[0]
 
     log(`${name} の最新バージョンが見つかりました!`)
@@ -15,10 +16,24 @@ async function getLatestVersion(type: string, name: string) {
     log(`URL: ${latest.package_html_url}`)
     log(`Update: ${latest.updated_at}`)
     log(`============`)
+
+    if (config.replaceVersion) 
+        replaceVersion(groupId, name, latest.name, config.buildFilePath)
+}
+
+function replaceVersion(groupId: string, packageName: string, version: string, path: string) {
+    const buildFile = fs.readFileSync(path, "utf8");
+    const replacedBuildFile = buildFile
+        .replace(
+            new RegExp(`implementation\\("${groupId.replace("\.", "\\.")}:${packageName}:.*"\\)`),
+            `implementation\("${groupId}:${packageName}:${version}"\)`
+        )
+    fs.writeFileSync(path, replacedBuildFile)
+    log(`${path} のバージョンを書き換えました!`)
 }
 
 function log(message: string) {
     console.log(`[CPV] ${message}`)
 }
 
-getLatestVersion(config.type, config.name)
+getLatestVersion(config.type, config.groupId, config.name)
